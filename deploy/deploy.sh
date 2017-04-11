c90bd301799ea2ba7ab4691bf10c52ca34005ef7
adesmarais <terodox@gmail.com>
2017-04-10 22:09:07 -0400

    A few fixes to get everything deploying.

---
+++ deploy/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+aws cloudformation package --template-file ./sam.yaml --output-template-file ./cloudformation.yaml --s3-bucket dev-iad-builds.newforma.cloud --s3-prefix adesmaraisNhJs/current/deploy
+aws cloudformation deploy --template-file ./cloudformation.yaml --stack-name adesmaraisNhJs --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
