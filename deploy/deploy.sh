091d43a2332abb4f4c034044bdfa9d770a7445f9
adesmarais <terodox@gmail.com>
2017-04-10 22:31:37 -0400

    Updated build, added script for build.
    Fixed notification setup for bucket.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+zip -r ../deploy/deploy.zip *
+cd ../deploy
 aws cloudformation package --template-file ./sam.yaml --output-template-file ./cloudformation.yaml --s3-bucket dev-iad-builds.newforma.cloud --s3-prefix adesmaraisNhJs/current/deploy
 aws cloudformation deploy --template-file ./cloudformation.yaml --stack-name adesmaraisNhJs --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
